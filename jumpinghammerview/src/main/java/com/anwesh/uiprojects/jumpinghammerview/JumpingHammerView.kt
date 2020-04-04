d7156da145a77e377354f56143af0583d4fd9f28
Anwesh43 <anweshthecool0@gmail.com>
2020-04-05 04:46:18 +0530

    adding constants and libraries

--- jumpinghammerview/src/main/java/com/anwesh/uiprojects/jumpinghammerview/JumpingHammerView.kt
+++ jumpinghammerview/src/main/java/com/anwesh/uiprojects/jumpinghammerview/JumpingHammerView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.jumpinghammerview
 /**
  * Created by anweshmishra on 05/04/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val bars : Int = 5
+val sizeFactor : Float = 1.5f
+val strokeFactor : Int = 90
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
