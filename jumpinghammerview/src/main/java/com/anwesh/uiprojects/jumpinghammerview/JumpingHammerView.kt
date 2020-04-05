31c43e6dfdb713bd3a677fdea0985367dcbc7408
Anwesh43 <anweshthecool0@gmail.com>
2020-04-05 05:40:39 +0530

    adding JHTNode class

--- jumpinghammerview/src/main/java/com/anwesh/uiprojects/jumpinghammerview/JumpingHammerView.kt
+++ jumpinghammerview/src/main/java/com/anwesh/uiprojects/jumpinghammerview/JumpingHammerView.kt
@@ -123,5 +123,47 @@ class JumpingHammerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JHTNode(var i : Int, val state : State = State()) {
+
+        private var next : JHTNode? = null
+        private var prev : JHTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = JHTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJHTNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JHTNode {
+            var curr : JHTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
 
